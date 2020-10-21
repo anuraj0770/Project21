96bb3d462d6f917168cc6228ce96b10c44da7057
anuraj0770 <72259779+anuraj0770@users.noreply.github.com>
2020-10-21 16:05:55 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -16,9 +16,7 @@ bullet1=createSprite(50,150,50,50)
 bullet1.velocityX=speed;
 
 wall=createSprite(1500,200,60,height/2)
-wall1=createSprite(1000,150,60,height/2)
-wall2=createSprite(500,100,60,height/2)
-wall3=createSprite(100,50,60,height/2)
+
 
 }
 
@@ -29,8 +27,9 @@ function draw() {
 if(wall.x-bullet.x < (bullet.width+wall.width)/2){
 bullet.velocityX=0;
 var deformation=0.5 * weight * speed * speed/22509;
+}
 if(deformation>180){
-  bullet.shapeColor=color(255,0,0)
+  wall.shapeColor=color(255,0,0)
 }
 
 if(deformation<180 && deformation>100){
@@ -38,42 +37,15 @@ bullet.shapeColor=color(230,230,0);
   }
 
 if(deformation<100){
-bullet.shapeColor=color(0,255,0);
-}
-}
-if (hasCollided(bullet,wall)){
-bullet.velocityX=0;
-var damage=0.5* weight*speed*speed/(thickness*thickness*thickness);
-
-if(damage>(10){
-wall.shapeColor=color(255,0,0);
-
-}
-
-}
-if(damage>10){
 wall.shapeColor=color(0,255,0);
-
+}
 }
 
-
-
-
-
-
-
-
-
-
-
-}
 function hasCollided(Lbullet,Lwall){
-    bulletRightEdge=lbullet.x+lbullet.width
-    wallLeftEdge=lwall.x
-    if(bulletRightEdge>=wallLeftEdge){
+bulletRightEdge=lbullet.x+lbullet.width
+wallLeftEdge=lwall.x
 
-        return true
-    }    
 
-  return false
+
+
 }
